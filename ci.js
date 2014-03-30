2512f0e248421a4634693490878f7865ee0b6f8d
Danilo Espino <daniloespino@gmail.com>
2014-03-30 15:58:53 -0700

    Update ci.js
    Arreglando bugs del validador.
    
    1. Cuando la suma de todos los dígitos te da 0, devolvías 10.
    2. El control que hacés ahí con los random que estás generando no deberías hacerlo solo con los de largo igual a 6 sino también los menores. Eso me generó algún NaN.

--- ci.js
+++ ci.js
@@ -1,13 +1,19 @@
 function validation_digit(ci){
   var a = 0;
   var i = 0;
-  if(ci.length == 6){
-    ci = '0' + ci;
+  if(ci.length <= 6){
+    for(i = ci.length; i < 7; i++){
+      ci = '0' + ci;
+    }
   }
   for(i = 0; i < 7; i++){
     a += (parseInt("2987634"[i]) * parseInt(ci[i])) % 10;
   }
-  return 10 - a % 10;
+  if(a%10 == 0){
+    return 0;
+  }else{
+    return 10 - a % 10;
+  }
 }
 
 function validate_ci(ci){
